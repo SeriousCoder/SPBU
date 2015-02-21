95ab8f61c4bc596276a040d38d761e65feda07a2
SeriousCoder <0kintol0@gmail.com>
2015-02-21 22:21:05 +0300

    Reorganisation folders. Add new projects
    Peano and BinTree

---
+++ 2 Sem/BinTree/BinTree/Program.fs
@@ -0,0 +1,70 @@
+﻿// Дополнительные сведения о F# см. на http://fsharp.net
+// Дополнительную справку см. в проекте "Учебник по F#".
+
+type elem = Nil | Info of elem * int * elem
+
+let rec  findMin t =
+   match t with
+   |Info(left, _ , _ ) when left = Nil -> t
+   |Info(left, _ , _ ) -> findMin left
+
+let rec add t a=
+   match t with
+   |Nil -> Info(Nil, a, Nil)
+   |Info(left, value, right) -> 
+      if a < value then Info(add left a, value, right) else Info(left, value, add right a)
+
+let rec del t a =
+   match t with
+   |Nil -> Nil
+   |Info(left, value, right) when a < value -> Info(del left a, value, right)
+   |Info(left, value, right) when value > a -> Info(left, value, del right a)
+   |Info(left, value, right) when value = a -> 
+      match left, right with
+      |Nil, Nil -> Nil
+      |Nil, _ -> right
+      | _ , Nil -> left
+      | _ , _ -> 
+         match findMin right with
+         |Nil -> Nil
+         |Info( _ , mValue, _ ) -> 
+            let right = del right mValue
+            Info(left, mValue, right)
+
+let rec clr t =
+   match t with
+   |Nil -> 0
+   |Info(left, _ , right) -> 
+      printf "%A\n" t
+      clr left |> ignore
+      clr right |> ignore
+      0
+
+let rec lrc t =
+   match t with
+   |Nil -> 0
+   |Info(left, _ , right) ->
+      lrc left |> ignore
+      lrc right |> ignore
+      printf "%A\n" t
+      0
+
+let rec lcr t =
+   match t with
+   |Nil -> 0
+   |Info(left, _ , right) ->
+      lcr left |> ignore
+      printf "%A\n" t
+      lcr right |> ignore
+      0
+
+[<EntryPoint>]
+let main argv = 
+   let mutable tree = Nil
+   tree <- add tree 3
+   tree <- add tree 1
+   tree <- add tree 5
+   tree <- add tree 4
+   tree <- del tree 3
+   lrc tree
+   0 // возвращение целочисленного кода выхода
---
+++ 2 Sem/Peano/FirstProblem/Program.fs
@@ -0,0 +1,38 @@
+﻿// Дополнительные сведения о F# см. на http://fsharp.net
+// Дополнительную справку см. в проекте "Учебник по F#".
+
+type Peano = Zero | S of Peano
+
+let rec plus a b =    //Сложение
+   match a with
+   |Zero -> b
+   |S a -> S (plus a b)
+
+let rec minus a b =   //Вычитание
+   match a, b  with
+   |Zero, _ -> Zero
+   |S a, S b -> minus a b
+   |S a, Zero -> S a
+
+let rec mult a b =   //Умножение
+   match a, b with
+   |Zero, _ -> Zero
+   | _ , Zero -> Zero
+   | S a, _ -> 
+      if a = Zero then b else plus b (mult a b)
+
+let rec pow a b =   //Возведение в степень
+   match b with
+   |Zero -> S Zero
+   |S b -> mult a (pow a b)  
+
+let rec toInt a =   //Перевод в Int
+   match a with
+   |Zero -> 0
+   |S a -> 1 + toInt a
+
+
+[<EntryPoint>]
+let main argv = 
+    printf "%A\n" (mult (S Zero) (S (S Zero)))
+    0 // возвращение целочисленного кода выхода
