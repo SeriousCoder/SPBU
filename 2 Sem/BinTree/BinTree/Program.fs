488fa8db1d2f648a9a39783a54220abef86e455d
SeriousCoder <0kintol0@gmail.com>
2015-03-02 01:34:53 +0300

    Edit fail with delete elements. Edit other bugs.

--- 2 Sem/BinTree/BinTree/Program.fs
+++ 2 Sem/BinTree/BinTree/Program.fs
@@ -5,7 +5,8 @@ type elem = Nil | Info of elem * int * elem
 
 let rec  findMin t =
    match t with
-   |Info(left, _ , _ ) when left = Nil -> t
+   |Info(left, value ,right) when left = Nil -> 
+      if right = Nil then value else findMin right
    |Info(left, _ , _ ) -> findMin left
 
 let rec add t a=
@@ -14,57 +15,85 @@ let rec add t a=
    |Info(left, value, right) -> 
       if a < value then Info(add left a, value, right) else Info(left, value, add right a)
 
+let rec CLRprint t =
+   match t with
+   |Nil -> 0
+   |Info(left, value , right) -> 
+      printf "%A " value
+      CLRprint left |> ignore
+      CLRprint right |> ignore
+      0
+
 let rec del t a =
    match t with
    |Nil -> Nil
    |Info(left, value, right) when a < value -> Info(del left a, value, right)
-   |Info(left, value, right) when value > a -> Info(left, value, del right a)
+   |Info(left, value, right) when value < a -> Info(left, value, del right a)
    |Info(left, value, right) when value = a -> 
       match left, right with
       |Nil, Nil -> Nil
       |Nil, _ -> right
       | _ , Nil -> left
-      | _ , _ -> 
-         match findMin right with
-         |Nil -> Nil
-         |Info( _ , mValue, _ ) -> 
-            let right = del right mValue
-            Info(left, mValue, right)
+      | mLeft , Info(xLeft, xValue , xRight ) -> if xLeft = Nil then Info(mLeft, xValue, xRight) 
+                                                 else Info(mLeft, findMin right, del right (findMin right))
 
-let rec clr t =
+//let rec del t a =
+//   match t with
+//   |Nil -> Nil
+//   |Info(left, value, right) -> if a < value then Info(del left a, value, right)
+//                                elif value < a then Info(left, value, del right a)
+//                                else match left, right with
+//                                     |Nil, Nil -> Nil
+//                                     |Nil, _ -> right
+//                                     | _ , Nil -> left
+//                                     | mLeft , Info(xLeft, xValue , xRight ) -> if xLeft = Nil then Info(mLeft, xValue, xRight) 
+//                                                                                else Info(mLeft, findMin right, del right (findMin right))
+
+
+let rec LRCprint t =
    match t with
    |Nil -> 0
-   |Info(left, _ , right) -> 
-      printf "%A\n" t
-      clr left |> ignore
-      clr right |> ignore
+   |Info(left, value , right) ->
+      LRCprint left |> ignore
+      LRCprint right |> ignore
+      printf "%A " value
       0
 
-let rec lrc t =
+let rec LCRprint t =
    match t with
    |Nil -> 0
-   |Info(left, _ , right) ->
-      lrc left |> ignore
-      lrc right |> ignore
-      printf "%A\n" t
-      0
-
-let rec lcr t =
-   match t with
-   |Nil -> 0
-   |Info(left, _ , right) ->
-      lcr left |> ignore
-      printf "%A\n" t
-      lcr right |> ignore
+   |Info(left, value , right) ->
+      LRCprint left |> ignore
+      printf "%A " value
+      LRCprint right |> ignore
       0
 
 [<EntryPoint>]
 let main argv = 
-   let mutable tree = Nil
-   tree <- add tree 3
-   tree <- add tree 1
-   tree <- add tree 5
-   tree <- add tree 4
-   tree <- del tree 3
-   lrc tree
+   let mutable tree = Info (Info(Info(Nil,1,Nil),3,Info(Info(Nil,4,Nil),6,Info(Nil,7,Nil))),8,Info(Nil,10,(Info(Info(Nil,13,Nil),14,Nil))))
+   printf "%A\n" tree
+   printf "LCR: "
+   LCRprint tree |> ignore
+   printf "\nLRC: "
+   LRCprint tree |> ignore
+   printf "\nCLR: "
+   CLRprint tree |> ignore
+   printf "\n"
+
+   tree <- del tree 8
+   tree <- del tree 7
+   printf "After delete 8 and 7: %A\n" tree
+   tree <- add tree 9
+   tree <- add tree 11
+   printf "After add 9 and 11: %A\n" tree
+   tree <- del tree 70
+
+   printf "%A\n" tree
+   printf "LCR: "
+   LCRprint tree |> ignore
+   printf "\nLRC: "
+   LRCprint tree |> ignore
+   printf "\nCLR: "
+   CLRprint tree |> ignore
+   printf "\n"
    0 // возвращение целочисленного кода выхода
--- 2 Sem/Peano/FirstProblem/Program.fs
+++ 2 Sem/Peano/FirstProblem/Program.fs
@@ -1,38 +1,38 @@
 ﻿// Дополнительные сведения о F# см. на http://fsharp.net
 // Дополнительную справку см. в проекте "Учебник по F#".
 
-type Peano = Zero | S of Peano
+type Peano = Zero | LoL of Peano
 
 let rec plus a b =    //Сложение
    match a with
    |Zero -> b
-   |S a -> S (plus a b)
+   |LoL a -> LoL (plus a b)
 
 let rec minus a b =   //Вычитание
    match a, b  with
    |Zero, _ -> Zero
-   |S a, S b -> minus a b
-   |S a, Zero -> S a
+   |LoL a, LoL b -> minus a b
+   |LoL a, Zero -> LoL a
 
 let rec mult a b =   //Умножение
    match a, b with
    |Zero, _ -> Zero
    | _ , Zero -> Zero
-   | S a, _ -> 
+   | LoL a, _ -> 
       if a = Zero then b else plus b (mult a b)
 
 let rec pow a b =   //Возведение в степень
    match b with
-   |Zero -> S Zero
-   |S b -> mult a (pow a b)  
+   |Zero -> LoL Zero
+   |LoL b -> mult a (pow a b)  
 
 let rec toInt a =   //Перевод в Int
    match a with
    |Zero -> 0
-   |S a -> 1 + toInt a
+   |LoL a -> 1 + toInt a
 
 
 [<EntryPoint>]
 let main argv = 
-    printf "%A\n" (mult (S Zero) (S (S Zero)))
+    printf "%A\n" (mult (LoL Zero) (LoL (LoL Zero)))
     0 // возвращение целочисленного кода выхода
