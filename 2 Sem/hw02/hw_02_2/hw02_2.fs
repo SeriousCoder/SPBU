43cf885a66692a17603c2b114234da1d7073145a
SeriousCoder <0kintol0@gmail.com>
2015-04-05 04:14:19 +0300

    Delete all bin files

---
+++ 2 Sem/hw02/hw_02_2/hw02_2.fs
@@ -0,0 +1,18 @@
+﻿// hw_02, part 2
+// by Tarasenko Nik, 171 group
+
+//let rec fold f acc list =
+//    match list with
+//    | [] -> acc
+//    | elem :: list -> fold f (f acc elem) list
+
+let horner a list =   //Реализация схемы Горнера
+    List.fold (fun acc elem ->  a * acc + elem) 0 list
+
+[<EntryPoint>]
+let main argv = 
+
+    horner 3 [2; -3; 7; 12] |> printfn "For x = 3 : 2x^3 - 3x^2 + 7x + 12 = %A"
+
+    0 // return an integer exit code
+
